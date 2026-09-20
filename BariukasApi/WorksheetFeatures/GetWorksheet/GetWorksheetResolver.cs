ed399a193774bc4f2de5394e629723ad02abaa86
taurasbear <youngicorn@gmail.com>
2026-09-20 18:19:54 +0300

    refactor: remove unused parameter

--- BariukasApi/WorksheetFeatures/GetWorksheet/GetWorksheetResolver.cs
+++ BariukasApi/WorksheetFeatures/GetWorksheet/GetWorksheetResolver.cs
@@ -3,7 +3,7 @@ using BariukasApi.Shared;
 namespace BariukasApi.WorksheetFeatures.GetWorksheet;
 
 [QueryType]
-public partial class GetWorksheetResolver(GetWorksheetHandler getWorksheetHandler, ILogger<GetWorksheetResolver> logger)
+public partial class GetWorksheetResolver(GetWorksheetHandler getWorksheetHandler)
 {
     public async Task<WorksheetDocument> GetWorksheet(Guid id, CancellationToken ct)
     {
